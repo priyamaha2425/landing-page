31cdefb59bc2b396a8c13a7dc58bcf102cb5a688
priyadharshini <priyadharshini9368@gmail.com>
2026-03-09 10:13:16 +0530

    deploy site 2

--- my-vue-app/vite.config.js
+++ my-vue-app/vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/landing-page/',
+  base: '/landing-page/my-vue-app',
   plugins: [vue()],
 })
